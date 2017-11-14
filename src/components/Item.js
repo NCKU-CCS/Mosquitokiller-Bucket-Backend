67337995378498bfc825164299f325cd7be90c79
Po Chun, Lu <oceanus11034@gmail.com>
2017-11-14 12:43:01 +0800

    [Add] basic item styled

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,10 +3,41 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { actions } from '../redux/places'
 
+import styled from 'styled-components'
+
 // ({onItemEdit, onFetchUpdateItem} = actions)
 
+const Row = styled.ul`
+  display: flex;
+  padding: 0;
+`
+
+const Column = styled.li`
+  list-style: none;  
+  padding: 0.5rem 1rem;
+  min-width: 7rem;
+  max-width: 9rem;
+  border-bottom: 1px solid gray;
+`
+
+const ColumnText = Column.extend`
+  text-align:left;
+`
+
+const ColumnNum = Column.extend`
+  text-align: right;
+`
+
 const Item = ({id, content}) => (
-  <li>{content.place_name}</li>
+  <Row>
+    <ColumnNum>{content.place_id}</ColumnNum>
+    <ColumnText>{content.place_name}</ColumnText>
+    <ColumnText>{content.place_address}</ColumnText>
+    <ColumnText>{content.place_contact_person}</ColumnText>
+    <ColumnNum>{content.place_phone}</ColumnNum>
+    <ColumnNum>{content.created_at}</ColumnNum>
+    <ColumnNum>{content.updated_at}</ColumnNum>
+  </Row>
 )
 
 export default Item
