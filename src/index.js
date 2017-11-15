9287de052e681d40012fe2c51ddd585d011384a0
Po Chun, Lu <oceanus11034@gmail.com>
2017-11-15 18:21:01 +0800

    [Add] lamps Router

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// Redux store
 import { Provider } from 'react-redux'
+import store, { history } from './store'
 
-import store from './store'
+// Route
+import { Route } from 'react-router'
+import { ConnectedRouter } from 'react-router-redux'
 
+// page Components
 import Places, { Lamps } from './containers/Page'
 import registerServiceWorker from './registerServiceWorker'
 
 
+
 ReactDOM.render(
   <Provider store={store}>
-    <Places />
+    <ConnectedRouter history={history}>
+      <div>
+        <Route exact path="/" component={Places}/>
+        <Route path="/lamps" component={Lamps}/>
+      </div>
+    </ConnectedRouter>
   </Provider>,
   document.getElementById('root'))
 
--- src/store.js
+++ src/store.js
@@ -4,12 +4,21 @@ import * as reducers from './redux'
 
 import middleWare from './middlewares'
 
+// Router
+import createHistory from 'history/createBrowserHistory'
+import { routerReducer, routerMiddleware } from 'react-router-redux'
+
+export const history = createHistory()
+const routeMiddleware = routerMiddleware(history)
+
+// Reducers
 const reducer = combineReducers({
-  ...reducers
+  ...reducers,
+  router: routerReducer
 })
 
 const store = createStore(reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(...middleWare))
+  applyMiddleware(...middleWare, routeMiddleware))
 
 export default store
